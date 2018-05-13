7bc758d37d8b1fdb95396695779e227c77d8930d
Nhan Cao <nhancv92@gmail.com>
2018-05-13 20:11:50 +0700

    Sudoku2

---
+++ nc-java/src/codefights/interview/arrays/Sudoku2.java
@@ -0,0 +1,115 @@
+package codefights.interview.arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * https://codefights.com/interview-practice/task/SKZ45AF99NpbnvgTn
+ * Sudoku is a number-placement puzzle. The objective is to fill a 9 × 9 grid with numbers in such a way that each column, each row, and each of the nine 3 × 3 sub-grids that compose the grid all contain all of the numbers from 1 to 9 one time.
+ * <p>
+ * Implement an algorithm that will check whether the given grid of numbers represents a valid Sudoku puzzle according to the layout rules described above. Note that the puzzle represented by grid does not have to be solvable.
+ * <p>
+ * Example
+ * <p>
+ * For
+ * <p>
+ * grid = [['.', '.', '.', '1', '4', '.', '.', '2', '.'],
+ * ['.', '.', '6', '.', '.', '.', '.', '.', '.'],
+ * ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+ * ['.', '.', '1', '.', '.', '.', '.', '.', '.'],
+ * ['.', '6', '7', '.', '.', '.', '.', '.', '9'],
+ * ['.', '.', '.', '.', '.', '.', '8', '1', '.'],
+ * ['.', '3', '.', '.', '.', '.', '.', '.', '6'],
+ * ['.', '.', '.', '.', '.', '7', '.', '.', '.'],
+ * ['.', '.', '.', '5', '.', '.', '.', '7', '.']]
+ * the output should be
+ * sudoku2(grid) = true;
+ * <p>
+ * For
+ * <p>
+ * grid = [['.', '.', '.', '.', '2', '.', '.', '9', '.'],
+ * ['.', '.', '.', '.', '6', '.', '.', '.', '.'],
+ * ['7', '1', '.', '.', '7', '5', '.', '.', '.'],
+ * ['.', '7', '.', '.', '.', '.', '.', '.', '.'],
+ * ['.', '.', '.', '.', '8', '3', '.', '.', '.'],
+ * ['.', '.', '8', '.', '.', '7', '.', '6', '.'],
+ * ['.', '.', '.', '.', '.', '2', '.', '.', '.'],
+ * ['.', '1', '.', '2', '.', '.', '.', '.', '.'],
+ * ['.', '2', '.', '.', '3', '.', '.', '.', '.']]
+ * the output should be
+ * sudoku2(grid) = false.
+ * <p>
+ * The given grid is not correct because there are two 1s in the second column. Each column, each row, and each 3 × 3 subgrid can only contain the numbers 1 through 9 one time.
+ * <p>
+ * Input/Output
+ * <p>
+ * [execution time limit] 3 seconds (java)
+ * <p>
+ * [input] array.array.char grid
+ * <p>
+ * A 9 × 9 array of characters, in which each character is either a digit from '1' to '9' or a period '.'.
+ * <p>
+ * [output] boolean
+ * <p>
+ * Return true if grid represents a valid Sudoku puzzle, otherwise return false.
+ */
+public class Sudoku2 {
+    char[][] grid =
+            {
+                    {'.', '.', '.', '.', '2', '.', '.', '9', '.'},
+                    {'.', '.', '.', '.', '6', '.', '.', '.', '.'},
+                    {'7', '1', '.', '.', '7', '5', '.', '.', '.'},
+                    {'.', '7', '.', '.', '.', '.', '.', '.', '.'},
+                    {'.', '.', '.', '.', '8', '3', '.', '.', '.'},
+                    {'.', '.', '8', '.', '.', '7', '.', '6', '.'},
+                    {'.', '.', '.', '.', '.', '2', '.', '.', '.'},
+                    {'.', '1', '.', '2', '.', '.', '.', '.', '.'},
+                    {'.', '2', '.', '.', '3', '.', '.', '.', '.'}
+            };
+
+
+    boolean sudoku2(char[][] grid) {
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                Set<Character> exists = new HashSet<Character>();
+                for (int k = 0; k < 3; ++k) {
+                    int row = i * 3 + k;
+                    for (int k2 = 0; k2 < 3; ++k2) {
+                        int col = j * 3 + k2;
+                        char c = grid[row][col];
+                        if (c != '.') {
+                            if (exists.contains(c)) {
+                                return false;
+                            }
+                            exists.add(c);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 9; ++i) {
+            Set<Character> exists1 = new HashSet<Character>();
+            Set<Character> exists2 = new HashSet<Character>();
+            for (int j = 0; j < 9; ++j) {
+                char c = grid[i][j];
+                if (c != '.') {
+                    if (exists1.contains(c)) {
+                        return false;
+                    }
+                    exists1.add(c);
+                }
+                char c2 = grid[j][i];
+                if (c2 != '.') {
+                    if (exists2.contains(c2)) {
+                        return false;
+                    }
+                    exists2.add(c2);
+                }
+            }
+        }
+
+        return true;
+    }
+}
