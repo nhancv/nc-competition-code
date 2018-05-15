31cf4c5c9caed92a7d6123efa1e67049daad3bd2
Nhan Cao <nhancv92@gmail.com>
2018-05-15 08:50:41 +0700

    add nc-dart

---
+++ nc-dart/bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:nc_dart/nc_dart.dart' as nc_dart;
+
+main(List<String> arguments) {
+  print('Hello world: ${nc_dart.calculate()}!');
+}
---
+++ nc-dart/test/nc_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:nc_dart/nc_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
