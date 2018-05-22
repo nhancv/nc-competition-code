89a3f23fde1e09ac6c2fea082e8efbded8819cf8
Nhan Cao <nhancv92@gmail.com>
2018-05-22 23:15:51 +0700

    update

--- nc-java/src/Main.java
+++ nc-java/src/Main.java
@@ -7,8 +7,9 @@ public class Main {
         CouldBeAnagram test = new CouldBeAnagram();
 //        System.out.println(test.couldBeAnagram("listen", "silent"));
 //        System.out.println(test.couldBeAnagram("ab??", "xyab"));
-        System.out.println(test.couldBeAnagram("a?by", "aaby"));
-//        System.out.println(test.couldBeAnagram("???z", "cool"));
+//        System.out.println(test.couldBeAnagram("a?by", "aaby"));
+//        System.out.println(test.couldBeAnagram("n?ce", "nice"));
+        System.out.println(test.couldBeAnagram("???z", "cool"));
 //        System.out.println(test.couldBeAnagram("coo?", "cool"));
     }
 
--- nc-java/src/codefights/challenge/CouldBeAnagram.java
+++ nc-java/src/codefights/challenge/CouldBeAnagram.java
@@ -1,37 +1,18 @@
 package codefights.challenge;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Stream;
 
 /**
  * https://codefights.com/challenge/hqLbYHvpTMZYYLA6h
  */
 public class CouldBeAnagram {
 
-    /**
-     * for (; i < q;) {
-     * t = s1.charAt(i);
-     * x = s2.charAt(i++);
-     * }
-     * int ra = 0, rb = 0;
-     * return ra - cb == 0 || rb + ca == 0;
-     */
-
-    int c[] = new int[125], n, q, t, x, i, g, u = 63;
+    int c[] = new int[125], q, i, g;
 
     public Object couldBeAnagram(String b, String v) {
         g = i = (q = b.length()) != v.length() ? q : 0;
-        while (i < q) {
-            t = b.charAt(i);
-            x = v.charAt(i++);
-            if (t > u) c[t]++;
-            else n++;
-            if (x > u) c[x]--;
-        }
-        for (int z : c) g += z > 0 ? z : -z;
-        return g == n;
+        for (; i < q; c[b.charAt(i)]++, c[v.charAt(i++)]--) ;
+        for (int z : c) g += z >= 0 ? z : -z;
+        return g == c[63] * 2;
     }
 
 }
