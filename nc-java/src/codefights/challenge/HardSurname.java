332426548f58a8819ee0ebd261ffeaa1359bb8ac
Nhan Cao <nhancv92@gmail.com>
2018-07-03 21:33:41 +0700

    update

--- nc-java/src/codefights/challenge/HardSurname.java
+++ nc-java/src/codefights/challenge/HardSurname.java
@@ -10,7 +10,7 @@ public class HardSurname {
     int m, n;
 
     public int hardSurname(String s) {
-        s.chars().forEach(v -> n = "ueoaiUEOAI".indexOf(v) < 0 ? m < ++n ? m = n : n : 0);
+        s.chars().forEach(v -> n = "ueoaiUA".indexOf(v) < 0 ? m < ++n ? m = n : n : 0);
         return m;
     }
 
