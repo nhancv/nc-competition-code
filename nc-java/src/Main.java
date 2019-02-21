67960ef8054227bc8df0a9b45ce6ca9c903b8f8c
Nhan Cao <nhancv92@gmail.com>
2019-02-21 16:37:01 +0700

    update

--- nc-java/src/Main.java
+++ nc-java/src/Main.java
@@ -3,14 +3,20 @@ import codefights.challenge.AreIsomorphic;
 public class Main {
 
     public static void main(String[] args) {
+//        AreIsomorphic test = new AreIsomorphic();
+//        int[][] array1 = new int[2][];
+//        array1[0] = new int[]{1, 1, 1};
+//        array1[1] = new int[]{0, 0};
+//        int[][] array2 = new int[2][];
+//        array2[0] = new int[]{2, 1, 1};
+//        array2[1] = new int[]{2, 1};
+//        System.out.println(test.areIsomorphic(array1, array2));
+
         AreIsomorphic test = new AreIsomorphic();
-        int[][] array1 = new int[2][];
-        array1[0] = new int[]{1, 1, 1};
-        array1[1] = new int[]{0, 0};
-        int[][] array2 = new int[2][];
-        array2[0] = new int[]{2, 1, 1};
-        array2[1] = new int[]{2, 1};
-        System.out.println(test.areIsomorphic(array1, array2));
+        System.out.println(test.pepEight(0));
+        for (int i = 79; i < 81; i++) {
+            System.out.println(test.pepEight(i));
+        }
     }
 
 }
--- nc-java/src/codefights/challenge/AreIsomorphic.java
+++ nc-java/src/codefights/challenge/AreIsomorphic.java
@@ -2,13 +2,14 @@ package codefights.challenge;
 
 public class AreIsomorphic {
 
-    Object pepEight(int l) {
-        return l < 80;
+    public int pepEight(int l) {
+        return l / 80 ^ 1;
     }
 
+    int i, x;
+
     public Object areIsomorphic(int[][] a, int[][] b) {
-        int i = 0, x = a.length;
-        for (; x == b.length & i < x && a[i].length == b[i].length; i++) ;
+        for (x = a.length; x == b.length & i < x && a[i].length == b[i].length; i++) ;
         return i == x;
     }
 
